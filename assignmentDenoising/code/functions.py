0883d570a82a93f55d864ce15a8b41de22dcd58a
krishnaw14 <krishnaw14@gmail.com>
2020-02-22 18:58:28 +0530

    Other prior functions and restructure results dir

--- assignmentDenoising/code/functions.py
+++ assignmentDenoising/code/functions.py
@@ -10,14 +10,21 @@ def likelihood_function(x,y):
 	return function_value, grad_value
 
 
-def quadratic_function(u):
+def quadratic_function(u, gamma):
 	function_value = np.sum(np.abs(u)**2)
 	grad_value = 2*u
 	return function_value, grad_value
  
 
-def discontinuity_adaptive_function(gamma):
-	pass
+def discontinuity_adaptive_function(u, gamma):
+	function_value = np.sum(gamma*np.abs(u) - (gamma**2)*np.log(1+np.abs(u)/gamma))
+	grad_value = gamma*np.sign(u) - gamma*np.sign(u)/(1+np.abs(u)/gamma)
 
-def discontinuity_adaptive_huber_function(gamma):
-	pass
+	return function_value, grad_value
+
+
+def discontinuity_adaptive_huber_function(u, gamma):
+	function_value = np.sum(0.5*(np.abs(u)**2)*(np.abs(u) <= gamma) + (gamma*np.abs(u) - 0.5*gamma**2)*(np.abs(u) > gamma))
+	grad_value = u*(np.abs(u) <= gamma) + gamma*np.sign(u)*(np.abs(u) > gamma)
+
+	return function_value, grad_value
