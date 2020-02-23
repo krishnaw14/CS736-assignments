00e019a408b0f02054492d8fb6ebdb51e73da9cf
krishnaw14 <krishnaw14@gmail.com>
2020-02-23 18:55:40 +0530

    Color Mode changes

--- assignmentDenoising/code/denoise.py
+++ assignmentDenoising/code/denoise.py
@@ -30,7 +30,7 @@ def calculate_posterior(x, y, prior_func, alpha, gamma=None):
 
 
 def denoise(noisy_img, denoised_img, alpha=0.6, gamma=0.5, optimize_mode=False, prior='quadratic', 
-	save_results_dir='../results/'):
+	save_results_dir='../results/', color_mode=False):
 	
 	prior_func = prior_func_dict[prior]
 	gamma = None if prior=='quadratic' else gamma
@@ -50,22 +50,11 @@ def denoise(noisy_img, denoised_img, alpha=0.6, gamma=0.5, optimize_mode=False, 
 	
 	counter = 0	
 	post_values = [initial_log_posterior]
-	while counter < 100 and step_size > 1e-8:
+	while counter < 150 and step_size > 1e-8:
 
 		# Monitor Objective Function. Partitioning the grid into sets such that a set contains no neighbours.
 
 		log_posterior, log_posterior_grad = calculate_posterior(x, y, prior_func, alpha, gamma)
-		# x[0:m:2, 0:n:2] = x[0:m:2, 0:n:2]-step_size*log_posterior_grad[0:m:2, 0:n:2]
-
-		# log_posterior, log_posterior_grad = calculate_posterior(x, y, prior_func, alpha, gamma)
-		# x[0:m:2, 1:n:2] = x[0:m:2, 1:n:2]-step_size*log_posterior_grad[0:m:2, 1:n:2]
-
-		# log_posterior, log_posterior_grad = calculate_posterior(x, y, prior_func, alpha, gamma)
-		# x[1:m:2, 0:n:2] = x[1:m:2, 0:n:2]-step_size*log_posterior_grad[1:m:2, 0:n:2]
-
-		# log_posterior, log_posterior_grad = calculate_posterior(x, y, prior_func, alpha, gamma)
-		# x[1:m:2, 1:n:2] = x[1:m:2, 1:n:2]-step_size*log_posterior_grad[1:m:2, 1:n:2]
-
 		x = x - step_size*log_posterior_grad
 
 		log_posterior, _ = calculate_posterior(x, y, prior_func, alpha, gamma)
@@ -77,16 +66,18 @@ def denoise(noisy_img, denoised_img, alpha=0.6, gamma=0.5, optimize_mode=False, 
 			counter += 1
 			post_values.append(log_posterior)
 		else:
-			step_size *= 0.5
 			x = x + step_size*log_posterior_grad
+			step_size *= 0.5
 
-		
 
 	if optimize_mode:
 		return rrmse(denoised_img, x)
 	else:
 		print('Initial RRMSE between Noisy and Denoised Image:', round(rrmse(denoised_img, noisy_img),5))
 		print('Denoising done in {} updates. RRMSE after denoising = {:.5f}'.format(counter, rrmse(denoised_img, x)) )
+
+		if color_mode: 
+			return x
 
 		plt.clf()
 		plt.plot(np.arange(counter+1), post_values)
@@ -106,15 +97,6 @@ def denoise(noisy_img, denoised_img, alpha=0.6, gamma=0.5, optimize_mode=False, 
 
 		plt.imshow(x, cmap=cmap)
 		plt.savefig(os.path.join(save_results_dir, '{}_prior_denoised_img.png'.format(prior)))
-
-		# import pdb; pdb.set_trace()
-
-		if not 'q1' in save_results_dir:
-			return x
-
-
-	
-
 
 
 
--- assignmentDenoising/code/grid_run.py
+++ assignmentDenoising/code/grid_run.py
@@ -1,22 +1,48 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
 from denoise import denoise
 from functions import *
 
-denoised_img = plt.imread('../data/mri_image_noiseless.png')
-low_noise_img = plt.imread('../data/mri_image_noise_level_low.png')
-med_noise_img = plt.imread('../data/mri_image_noise_level_medium.png')
-high_noise_img = plt.imread('../data/mri_image_noise_level_high.png')
+# denoised_img = plt.imread('../data/mri_image_noiseless.png')
+# low_noise_img = plt.imread('../data/mri_image_noise_level_low.png')
+# med_noise_img = plt.imread('../data/mri_image_noise_level_medium.png')
+# high_noise_img = plt.imread('../data/mri_image_noise_level_high.png')
 
-rrmse_final_min = 100
-beta_opt = 0
-for beta in np.linspace(0.5,0.9,30):
-	for gamma in np.linspace(0.001,0.05,30):
-		rrmse_final = denoise(low_noise_img, denoised_img, alpha=beta, gamma=gamma, optimize_mode=True, prior='discontinuity_adaptive')
-		if rrmse_final < rrmse_final_min:
-			rrmse_final_min = rrmse_final
-			beta_opt = beta
-			gamma_opt = gamma
+denoised_img = cv2.cvtColor(plt.imread('../data/histology_noiseless.png'), cv2.COLOR_BGR2HSV)
+noisy_img = cv2.cvtColor(plt.imread('../data/histology_noisy.png'), cv2.COLOR_BGR2HSV)
 
-print('rrmse_min = {}, beta_opt = {}, gamma_opt = {}'.format(rrmse_final_min, beta_opt, gamma_opt))
+# rrmse_final_min = 100
+# beta_opt = 0
+# for beta in np.linspace(0,1,40):
+# 	for gamma in np.linspace(0.001,1,40):
+# 		rrmse_final = denoise(noisy_img[:,:,2], denoised_img[:,:,2], alpha=beta, gamma=gamma, optimize_mode=True, prior='discontinuity_adaptive_huber')
+# 		if rrmse_final < rrmse_final_min:
+# 			rrmse_final_min = rrmse_final
+# 			beta_opt = beta
+# 			gamma_opt = gamma
+
+# rrmse_init_channel = rrmse(denoised_img[:,:,2], noisy_img[:,:,2])
+# rrmse_init_total = rrmse(denoised_img, noisy_img)
+
+# print('rrmse_min = {}, beta_opt = {}, gamma_opt = {}'.format(rrmse_final_min, beta_opt, gamma_opt))
+
+channel_0 = denoise(noisy_img[:,:,0], denoised_img[:,:,0], alpha=1, gamma=1, optimize_mode=False, prior='discontinuity_adaptive_huber', 
+	color_mode=True)
+
+channel_1 = denoise(noisy_img[:,:,1], denoised_img[:,:,1], alpha=0.8974, gamma=0.02662, optimize_mode=False, prior='discontinuity_adaptive_huber', 
+	color_mode=True)
+
+channel_2 = denoise(noisy_img[:,:,2], denoised_img[:,:,2], alpha=0.8718, gamma=0.02662, optimize_mode=False, prior='discontinuity_adaptive_huber', 
+	color_mode=True)
+
+sample = noisy_img.copy()
+sample[:,:,0] = channel_0
+sample[:,:,1] = channel_1
+sample[:,:,2] = channel_2
+
+plt.imshow( noisy_img); plt.savefig('noisy.png'); plt.clf()
+plt.imshow( denoised_img); plt.savefig('noiseless.png'); plt.clf()
+plt.imshow( sample); plt.savefig('sample.png'); plt.clf()
+
 import pdb; pdb.set_trace()
