db037ad252b50de4e77a34f067e35cbe652bc375
Vipul Ramtekkar <vipul.ramtekkar@missouri.edu>
2020-04-03 21:19:47 +0530

    q2

---
+++ assignmentReconstruction/code/denoise.py
@@ -0,0 +1,101 @@
+import numpy as np 
+import os
+import matplotlib.pyplot as plt
+from functions import *
+
+prior_func_dict = {
+	'quadratic': quadratic_function,
+	'discontinuity_adaptive': discontinuity_adaptive_function,
+	'discontinuity_adaptive_huber': discontinuity_adaptive_huber_function
+}
+
+def calculate_posterior(x, y, prior_func, alpha, gamma=None):
+
+	def calculate_prior(x):
+		prior_1, grad_1 = prior_func(x - np.roll(x, 1, axis=0), gamma)
+		prior_2, grad_2 = prior_func(x - np.roll(x, -1, axis=0), gamma)
+		prior_3, grad_3 = prior_func(x - np.roll(x, 1, axis=1), gamma)
+		prior_4, grad_4 = prior_func(x - np.roll(x, -1, axis=1), gamma)
+
+		prior = prior_1 + prior_2 + prior_3 + prior_4
+		grad = grad_1 + grad_2 + grad_3 + grad_4
+		return prior, grad
+
+	likelihood, likelihood_grad = likelihood_function(x,y)
+	prior, prior_grad = calculate_prior(x)
+	posterior = alpha*prior + (1-alpha)*likelihood
+	posterior_grad = alpha*prior_grad + (1-alpha)*likelihood_grad
+
+	return posterior, posterior_grad
+
+
+def denoise(noisy_img, denoised_img, alpha=0.6, gamma=0.5, optimize_mode=False, prior='quadratic', 
+	save_results_dir='../results/', color_mode=False):
+	
+	prior_func = prior_func_dict[prior]
+	gamma = None if prior=='quadratic' else gamma
+
+	# save_results_dir = os.path.join(save_results_dir, prior)
+	os.makedirs(save_results_dir, exist_ok=True)
+		
+	m, n = denoised_img.shape
+
+	# Using Notation used in class
+	x = noisy_img.copy() # Initial Estimate
+	y = noisy_img.copy() # Observed Data
+
+	step_size = 1e-2 # Initial Learning Rate
+
+	initial_log_posterior, _ = calculate_posterior(x, y, prior_func, alpha, gamma)
+	
+	counter = 0	
+	post_values = [initial_log_posterior]
+	while counter < 150 and step_size > 1e-8:
+
+		# Monitor Objective Function. Partitioning the grid into sets such that a set contains no neighbours.
+
+		log_posterior, log_posterior_grad = calculate_posterior(x, y, prior_func, alpha, gamma)
+		x = x - step_size*log_posterior_grad
+
+		log_posterior, _ = calculate_posterior(x, y, prior_func, alpha, gamma)
+
+		# Dynamic Learning Rate
+		if log_posterior/initial_log_posterior < 1:
+			step_size *= 1.1
+		else:
+			step_size *= 0.5
+
+		initial_log_posterior = log_posterior.copy()
+		counter += 1
+		post_values.append(log_posterior)
+
+
+	if optimize_mode:
+		return rrmse(denoised_img, x)
+	else:
+		print('Initial RRMSE between Noisy and Denoised Image:', round(rrmse(denoised_img, noisy_img),5))
+		print('Denoising done in {} updates. RRMSE after denoising = {:.5f}'.format(counter, rrmse(denoised_img, x)) )
+
+		plt.clf()
+		plt.plot(np.arange(counter+1), post_values)
+		plt.xlabel('Number of iterations')
+		plt.ylabel('Objective Function (log)')
+		plt.title('{} prior'.format(prior))
+		plt.savefig(os.path.join(save_results_dir, 'objective_function.png'))
+		plt.clf()
+
+		cmap = 'gray' if 'q1' in save_results_dir else None
+
+		plt.imshow(noisy_img, cmap=cmap)
+		plt.savefig(os.path.join(save_results_dir, 'noisy_img.png'))
+
+		plt.imshow(denoised_img, cmap=cmap)
+		plt.savefig(os.path.join(save_results_dir, 'ground_turth_denoised_img.png'))
+
+		plt.imshow(x, cmap=cmap)
+		plt.savefig(os.path.join(save_results_dir, '{}_prior_denoised_img.png'.format(prior)))
+
+		if color_mode: 
+			return x, post_values
+
+
--- assignmentReconstruction/code/q2.py
+++ assignmentReconstruction/code/q2.py
@@ -2,29 +2,73 @@
 #Rollno: 16D110013
 #Assignment 3: Image Reconstruction
 #References: https://scikit-image.org/docs/dev/auto_examples/transform/plot_radon_transform.html
-
+import os 
 import numpy as np 
 import cv2 
 import matplotlib.pyplot as plt
-from skimage.transform import radon
+from skimage.transform import radon, iradon
 from helper_functions import get_rrmse
-from filtered_backprojection import *
+from functions import *
+from denoise import denoise
+from filtered_backprojection import filtered_backprojection
 
+save_results_dir = '../results/q2/'
 
 ChestPhantom = plt.imread("../data/ChestPhantom.png")
 print (ChestPhantom.shape)
 theta = np.linspace(start = 0, stop = 180,num = 180,endpoint = False, dtype = int)
-signogram = radon(ChestPhantom, theta, circle = True)
+sinogram = radon(ChestPhantom, theta, circle = True)
+# Solving for A (High computational complexity)
 
-A = np.linalg.solve(ChestPhantom.reshape(-1,1),signogram.reshape(-1,1))
 
-diff = np.max(signogram) - np.min(signogram)
-noise = np.random.normal(0,diff*0.02,signogram.shape)
-signogram_withnoise = signogram + noise
 
-filtered_backprojection(signogram_withnoise)
+#A = np.linalg.lstsq(ChestPhantom.reshape(-1,1),sinogram.reshape(-1,1))
+
+diff = np.max(sinogram) - np.min(sinogram)
+noise = np.random.normal(0,diff*0.02,sinogram.shape)
+sinogram_withnoise = sinogram + noise
+
+img_recon = iradon(sinogram_withnoise, theta)
 
 #part d Tikhonov 
+from sklearn.linear_model import Ridge 
+
+clf = Ridge(alpha = 1.0)
+clf.fit(img_recon,ChestPhantom)
+Ridge()
 
 
+# Part e 
 
+denoise(img_recon, ChestPhantom, alpha=0.0875, optimize_mode=False, prior='quadratic', 
+	save_results_dir=os.path.join(save_results_dir, 'quadratic', 'low_noise_level'))
+print('RRMSE at 1.2 times optimum alpha=', round(denoise(img_recon, ChestPhantom, alpha=0.0875*1.2, optimize_mode=True, prior='quadratic', 
+	save_results_dir=os.path.join(save_results_dir, 'quadratic', 'low_noise_level')), 5))
+print('RRMSE at 0.8 times optimum alpha=', round(denoise(img_recon, ChestPhantom, alpha=0.0875*0.8, optimize_mode=True, prior='quadratic', 
+	save_results_dir=os.path.join(save_results_dir, 'quadratic', 'low_noise_level')), 5))
+print('---------------------------------------------------------------------------------\n')
+
+
+denoise(img_recon, ChestPhantom, alpha=0.8748, gamma=0.0023, optimize_mode=False, prior='discontinuity_adaptive', 
+	save_results_dir=os.path.join(save_results_dir, 'discontinuity_adaptive', 'low_noise_level'))
+print('RRMSE at 1.2 times optimum alpha=', round(denoise(img_recon, ChestPhantom, alpha=0.8748*1.2, gamma=0.0023, optimize_mode=True, prior='discontinuity_adaptive', 
+	save_results_dir=os.path.join(save_results_dir, 'discontinuity_adaptive', 'low_noise_level')), 5))
+print('RRMSE at 0.8 times optimum alpha=', round(denoise(img_recon, ChestPhantom, alpha=0.8748*0.8, gamma=0.0023, optimize_mode=True, prior='discontinuity_adaptive', 
+	save_results_dir=os.path.join(save_results_dir, 'discontinuity_adaptive', 'low_noise_level')), 5))
+print('RRMSE at 1.2 times optimum gamma=', round(denoise(img_recon, ChestPhantom, alpha=0.8748, gamma=0.0023*1.2, optimize_mode=True, prior='discontinuity_adaptive', 
+	save_results_dir=os.path.join(save_results_dir, 'discontinuity_adaptive', 'low_noise_level')), 5))
+print('RRMSE at 0.8 times optimum gamma=', round(denoise(img_recon, ChestPhantom, alpha=0.8748, gamma=0.0023*0.8, optimize_mode=True, prior='discontinuity_adaptive', 
+	save_results_dir=os.path.join(save_results_dir, 'discontinuity_adaptive', 'low_noise_level')), 5))
+
+
+denoise(img_recon, ChestPhantom, alpha=0.8748, gamma=0.00211, optimize_mode=False, prior='discontinuity_adaptive_huber', 
+	save_results_dir=os.path.join(save_results_dir, 'discontinuity_adaptive_huber', 'low_noise_level'))
+print('RRMSE at 1.2 times optimum alpha=', round(denoise(img_recon, ChestPhantom, alpha=0.8748*1.2, gamma=0.00211, optimize_mode=True, prior='discontinuity_adaptive_huber', 
+	save_results_dir=os.path.join(save_results_dir, 'discontinuity_adaptive_huber', 'low_noise_level')), 5))
+print('RRMSE at 0.8 times optimum alpha=', round(denoise(img_recon, ChestPhantom, alpha=0.8748*0.8, gamma=0.00211, optimize_mode=True, prior='discontinuity_adaptive_huber', 
+	save_results_dir=os.path.join(save_results_dir, 'discontinuity_adaptive_huber', 'low_noise_level')), 5))
+print('RRMSE at 1.2 times optimum gamma=', round(denoise(img_recon, ChestPhantom, alpha=0.8748, gamma=0.00211*1.2, optimize_mode=True, prior='discontinuity_adaptive_huber', 
+	save_results_dir=os.path.join(save_results_dir, 'discontinuity_adaptive_huber', 'low_noise_level')), 5))
+print('RRMSE at 0.8 times optimum gamma=', round(denoise(img_recon, ChestPhantom, alpha=0.8748, gamma=0.00211*0.8, optimize_mode=True, prior='discontinuity_adaptive_huber', 
+	save_results_dir=os.path.join(save_results_dir, 'discontinuity_adaptive_huber', 'low_noise_level')), 5))
+print('---------------------------------------------------------------------------------\n')
